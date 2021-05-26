a11ff57dcb2b7014e545f8c3352b610c4f7e86cd
Tobias <schindus89@gmx.de>
2021-05-26 14:59:59 +0200

    test as

---
+++ src/TestAS.java
@@ -0,0 +1,28 @@
+import AntSystem.AS;
+
+import java.util.Arrays;
+
+public class TestAS {
+    public static void main(String[] args) {
+        // initialize the Ant System
+        AS as = new AS(AS.createRandomVertices(10, 100), 10);
+        // printMatrix(as.graph.distanceMatrix);
+
+        // set parameter
+        as.setNumberOfIterations(1000);
+        as.setAlpha(1.0);
+        as.setBeta(1.0);
+        as.setRho(0.01);
+
+        // solve and measure time
+        long start = System.currentTimeMillis();
+        as.solve();
+        long finish = System.currentTimeMillis();
+        long compTime = finish - start;
+
+        // print result
+        System.out.println(Arrays.toString(as.getBestTour()));
+        System.out.println(as.getBestTourLength());
+        System.out.printf("compTime: %f s", compTime / 1000.0);
+    }
+}
