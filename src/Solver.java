bd0bd1c1fcdf8b36134fa4d055a01c74e0362a07
Tobias <schindus89@gmx.de>
2021-06-18 15:39:02 +0200

    update

--- src/Solver.java
+++ src/Solver.java
@@ -11,6 +11,7 @@ public class Solver {
     public static void main(String[] args) {
         int toIndex = 0;
         int numOfSolutions = 10;
+        String directoryOfTheFiles = "/home/tobias/Programmierung/Java/AntAlgorithm/src/Files/";
 
         // parameter
         double alpha = 1;
@@ -22,21 +23,19 @@ public class Solver {
         int numIt = 1000;
 
         // runs the Ant System
-        RunAS runAS = new RunAS("/home/tobias/Programmierung/Java/AntAlgorithm/src/Files/",
-                numOfSolutions, alpha, beta, rho, tau0, numIt);
+        RunAS runAS = new RunAS(directoryOfTheFiles, numOfSolutions, alpha, beta, rho, tau0, numIt);
         // runAS.solveAllFiles();
         for (int i = 0; i <= toIndex; i++)
             runAS.solveOneFile(i);
 
         // runs the Ant Colony System
-        RunACS runACS = new RunACS("/home/tobias/Programmierung/Java/AntAlgorithm/src/Files/",
-                numOfSolutions, alpha, beta, rho, tau0, numIt, q0, phi);
+        RunACS runACS = new RunACS(directoryOfTheFiles, numOfSolutions, alpha, beta, rho, tau0, numIt, q0, phi);
         // runACS.solveAllFiles();
         for (int i = 0; i <= toIndex; i++)
             runACS.solveOneFile(i);
 
         // runs the nearest neighbour heuristic
-        RunNNH runNNH = new RunNNH("/home/tobias/Programmierung/Java/AntAlgorithm/src/Files/");
+        RunNNH runNNH = new RunNNH(directoryOfTheFiles);
         runNNH.solveAllFiles();
         // runNNH.solveOneFile(index);
     }
